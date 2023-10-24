d59e0236670faaea0f5703944aa599707ebcff32
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-10-23 22:00:19 -0300

    req 4 espero

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Profile(models.Model):
     name = models.CharField(max_length=50)
     github = models.URLField(max_length=500)
@@ -9,13 +10,16 @@ class Profile(models.Model):
     def _str_(self) -> str:
         return self.name
 
+
 class Project(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
     github_url = models.URLField(max_length=500)
     keyword = models.TextField(max_length=50)
     key_skill = models.TextField(max_length=50)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="Projects")
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="Projects"
+    )
 
     def _str_(self) -> str:
         return self.name
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404, render
 from projects.models import Profile, Project
 from rest_framework import viewsets
 from projects.serializer import (
@@ -7,7 +8,7 @@ from projects.serializer import (
     ProjectWriteSerializer,
 )
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -15,6 +16,17 @@ class ProfileViewSet(viewsets.ModelViewSet):
     serializer = ProfileReadOnlySerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == "GET":
+            profile = get_object_or_404(Profile, pk=kwargs.get("pk"))
+            return render(request, "profile_detail.html", {"profile": profile})
+        return super().retrieve(request, *args, **kwargs)
 
     def get_serializer_class(self):
         if self.action in ["read"]:
